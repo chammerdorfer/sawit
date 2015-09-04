0741f44bdb49628dd6750fe0bf349106a7e4eb61
Christian Hammerdorfer <chammerdorfer@gmail.com>
2015-09-04 01:46:52 -0600

    third

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
 	def index
+		# @per_page = params[:per_page] ||= 5
+		# @page = params[:page] ||=1
+		@links = Link.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
 	end
 end
